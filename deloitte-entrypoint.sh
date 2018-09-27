70987df44663cc93b0c8c7d4b699d6cc1c583f0d
Yusuf Hasan Miyan <yusufh@protonmail.com>
2018-09-27 16:38:19 +1000

    Updated info message when changing uid.

--- deloitte-entrypoint.sh
+++ deloitte-entrypoint.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 # Change user & group id if user id provided.
 if [ ! -z "${DELOITTE_USER_ID}" ]
 then
-  echo "Starting with UID : ${DELOITTE_USER_ID}"
+  echo "Changing deloitte's UID to: ${DELOITTE_USER_ID}"
   usermod -u ${DELOITTE_USER_ID} deloitte
   groupmod -g ${DELOITTE_USER_ID} deloitte
   find / -not -path "/proc/*" -group 1000 -exec chgrp -h deloitte {} \;
