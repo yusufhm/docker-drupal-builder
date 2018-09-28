93c1a057354cc93f66f99dfdfe79859022f76924
Yusuf Hasan Miyan <yusufh@protonmail.com>
2018-09-28 17:36:10 +1000

    Set MYSQL_ALLOW_EMPTY_PASSWORD in entrypoint.

--- deloitte-entrypoint.sh
+++ deloitte-entrypoint.sh
@@ -4,13 +4,14 @@ set -eo pipefail
 # Change user & group id if user id provided.
 if [ ! -z "${DELOITTE_USER_ID}" ]
 then
-  echo "Changing deloitte's UID to: ${DELOITTE_USER_ID}"
-  usermod -u ${DELOITTE_USER_ID} deloitte
-  groupmod -g ${DELOITTE_USER_ID} deloitte
+  echo "Changing deloitte's UID to: ${DELOITTE_USER_ID}";
+  usermod -u ${DELOITTE_USER_ID} deloitte;
+  groupmod -g ${DELOITTE_USER_ID} deloitte;
   find / -not -path "/proc/*" -group 1000 -exec chgrp -h deloitte {} \;
 fi
 
 # Run mariadb's entrypoint.
+export MYSQL_ALLOW_EMPTY_PASSWORD=true;
 /usr/local/bin/docker-entrypoint.sh mysqld &> /tmp/mysql.log &
 
 exec "$@"
